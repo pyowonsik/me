2302f9ccfc58e6adb9ac636fdb2d0e57bf7a88a7
pyowonsik <qqrtyu@gmail.com>
2024-01-22 06:05:11 +0900

    init : me

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:me/src/app.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const App();
+  }
+}
