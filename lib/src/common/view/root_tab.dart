4d73e6f18a5101163860e5b75c3839dc2e823210
pyowonsik <qqrtyu@gmail.com>
2024-01-24 06:35:10 +0900

    feat : root tab

--- lib/src/common/view/root_tab.dart
+++ lib/src/common/view/root_tab.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:me/src/common/layout/default_layout.dart';
 import 'package:me/src/common/provider/router_provider.dart';
+import 'package:me/src/diary/view/diary.dart';
 
 class RootTab extends ConsumerStatefulWidget {
   const RootTab({super.key});
@@ -9,20 +11,56 @@ class RootTab extends ConsumerStatefulWidget {
   ConsumerState<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends ConsumerState<RootTab> {
+class _RootTabState extends ConsumerState<RootTab>
+    with SingleTickerProviderStateMixin {
+  var index = 0;
+  late TabController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+    controller.addListener(tabListenr);
+  }
+
+  void tabListenr() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Root Tab"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(routerProvider).go('/write_diary');
-        },
-        child: Icon(Icons.edit), // "글쓰기"와 관련된 아이콘 사용
-        tooltip: '글쓰기', // 마우스를 버튼 위에 가져다 놓을 때 표시될 툴팁
-      ),
+    return DefaultLayout(
+      title: 'Root Tab',
+      body: TabBarView(
+          controller: controller,
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            Container(),
+            Diary(),
+          ]),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedFontSize: 10,
+          unselectedFontSize: 10,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            controller.animateTo(index);
+          },
+          currentIndex: index,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.edit_outlined), label: '일기'),
+          ]),
     );
   }
 }
